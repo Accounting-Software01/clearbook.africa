bc846778391872627ff81f3cdb3de692cbd2711f
Accounting-Software01 <mshamisu@proton.me>
2026-03-12 12:25:28 +0100

    Update page.js

--- src/app/(app)/account-payable/page.js
+++ src/app/(app)/account-payable/page.js
@@ -1,9 +1,1 @@
-// app/(app)/account-payable/page.js
-export default function AccountPayablePage() {
-    return (
-        <div>
-            <h1>Account Payable</h1>
-            {/* You can later add your AccountsPayableTabContent here */}
-        </div>
-    );
-}
+
