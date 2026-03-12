b10b74b7def173eb072b6b8ac527465f42926a6f
msabdhallah <msabdhallah@gmail.com>
2026-03-12 13:42:03 +0000

    Updates

--- docs/sql/goods_received_note_items.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE goods_received_note_items (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    grn_id INT NOT NULL,
-    po_item_id INT NOT NULL,
-    quantity_received DECIMAL(12,2) NOT NULL,
-
-    FOREIGN KEY (grn_id) REFERENCES goods_received_notes(id),
-    FOREIGN KEY (po_item_id) REFERENCES purchase_order_items(id)
-);
